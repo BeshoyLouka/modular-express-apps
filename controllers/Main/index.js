ca43f1fe17c94f30214fd99304fbab61cb39d9ae
Beshoy Louka <beshoy.louka@gmail.com>
2014-04-01 16:22:50 -0400

    init controlers and routes

--- controllers/Main/index.js
+++ controllers/Main/index.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var app = module.exports = express();
 
-app.set('views', __dirname);
-
 app.get('/', function(req, res){
-    res.render('./views/main');
+    res.render('main');
 });
---
+++ controllers/Patients/index.js
@@ -0,0 +1,3 @@
+exports.list = function(req, res){
+    res.render('patients/list', { title: 'Route Separation Example' });
+};
